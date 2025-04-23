2f550cdfc040ed64fe3d9c97d78e75c37be36af7
Teresa Huang <thuang@workergpu092.cm.cluster>
2025-04-22 22:46:36 -0400

    update build_PyG_fromh5

--- graph_util.py
+++ graph_util.py
@@ -106,8 +106,8 @@ def build_PyGdata_velocity(data_path, graph_path, r_link, p=1):
         graph = Data(x=torch.tensor(Xs[i], dtype=torch.float32).unsqueeze(1), #(N, 1, 3)
                     y=torch.tensor(ys[i], dtype=torch.float32).unsqueeze(1), #(N, 1, 3)
                     edge_index=torch.tensor(edge_index.T, dtype=torch.long), #(2, E) in PyG format
-                    edge_attr=torch.tensor(edge_dist/r_link**2, dtype=torch.float32).unsqueeze(1), #(E, k) in PyG format
-                    node_weight = node_weight.unsqueeze(1))
+                    edge_attr=torch.tensor(edge_dist/r_link**2, dtype=torch.float32).unsqueeze(1).unsqueeze(2), #(E, 1, 1) in PyG format
+                    node_weight = node_weight.unsqueeze(1).unsqueeze(2))
         PyGdataset.append(graph)
     return PyGdataset
 
@@ -127,7 +127,7 @@ def split_dataloader(dataset, batch_size=2000, shuffle=True):
     return train_loader, valid_loader, test_loader
 
 
-def build_PyGdata_fromH5(h5_path, predict_velocity=False, data_name='BSQ',
+def build_PyGdata_fromH5(h5_path, predict_velocity=False, data_name='LH',
                  edge_flag=False, r_link=50.,
                  L=1000, leafsize=16, epsilon=0.00001):
     ''' 
@@ -143,6 +143,7 @@ def build_PyGdata_fromH5(h5_path, predict_velocity=False, data_name='BSQ',
     with h5py.File(h5_path, 'r') as f:
         group = f[data_name]
         labels = f["params"]
+        print(len(list(group.keys())))
         for graph_name in group:
             # start = time.time()
             g = group[graph_name] #of the form '/BSQ/BSQ_0'
@@ -165,15 +166,10 @@ def build_PyGdata_fromH5(h5_path, predict_velocity=False, data_name='BSQ',
             else:
             # Read the cloud-level labels: cosmological parameters:
                 # TODO -- fix the names across different h5 files
-                if data_name == 'BSQ':
-                    Omega_m = labels['Omega_m'][idx]
-                    sigma_8 = labels['sigma_8'][idx]
-                    Omega_b = labels['Omega_b'][idx]
-                    h = labels['h'][idx]
-                    n_s = labels['n_s'][idx]
-                    y = torch.tensor([Omega_m, sigma_8, Omega_b, h, n_s], dtype=torch.float).view(-1,1)
-                else:
-                    y = torch.tensor(labels['sigma8'][idx], dtype=float).view(-1,1)
+                #if data_name == 'BSQ':
+                Omega_m = np.array(labels['Omega_m'][:]).reshape(-1,1)
+                sigma_8 = np.array(labels['sigma_8'][:]).reshape(-1,1)
+                y = np.hstack((Omega_m, sigma_8))
 
             # create edges based on position
             if edge_flag:
@@ -206,9 +202,10 @@ if __name__ == '__main__':
     #                     default='datasets/position_only/graph_m=2000_n=5000.pkl', help='graph dataset file')
     parser.add_argument('--r_link', type=float, default=90, help='knn graph distance cut-off threshold')
     parser.add_argument('--neutrinos', action='store_true', help='use neutrino dataset')
-    parser.add_argument('--h5_path', default='/mnt/home/rstiskalek/ceph/CAMELS-SAM/LH_rockstar_99.hdf5',
+    parser.add_argument('--h5_path', #default='/mnt/home/rstiskalek/ceph/CAMELS-SAM/LH_rockstar_99.hdf5',
+                        default='/mnt/home/rstiskalek/ceph/CAMELS-SAM/LH_rockstar_99_top5000_train_lhs.hdf5',
                          help='h5 path to load the data')
-    parser.add_argument('--data_name', default='BSQ',
+    parser.add_argument('--data_name', default='LH', type=str,
                          help='data group name in the h5 file') #TODO: sync across BSQ and LH? 
 
     #the same script applies to position_only and position_velocity point clouds, 
